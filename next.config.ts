7c6c9a69f18c05709ba8af57efc954cdb46f16c5
yeooonn <rladuswjd0303@naver.com>
2025-05-06 01:27:50 +0900

    Chore: shadcn select ui 설치

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
